b13738f65c323a9be030bf9f42ea3c603aacc040
rocketmanhs <31834217+rocketmanhs@users.noreply.github.com>
2018-01-27 15:43:16 -0800

    gamemanager

--- GGJ_2018_Project/Assets/_Scripts/Game Management/Goal_Controller.cs
+++ GGJ_2018_Project/Assets/_Scripts/Game Management/Goal_Controller.cs
@@ -6,12 +6,16 @@ namespace GGJ_2018_AP
 {
     public class Goal_Controller : MonoBehaviour
     {
-        Game_Manager goalReached = new Game_Manager();
+        public GameObject WinPanel;
 
+        Game_Manager goalReached = new Game_Manager();
+        
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            Debug.Log("YOU WIN!!!!!");
+            goalReached.YouWin(WinPanel);
             goalReached.StopGameTime();
-            goalReached.YouWin();
+
         }
 
     }
