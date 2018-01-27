46dd4c1d02fdc8c3b409a3acdc9d122ce1c34c44
Joel Lee <lee.joel395@gmail.com>
2018-01-27 14:44:49 -0800

    Added rough Camera work. Added sprite depth simulant.

---
+++ GGJ_2018_Project/Assets/_Scripts/SpriteDepth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteDepth : MonoBehaviour {
+    private SpriteRenderer _sr;
+    void Awake() {
+        _sr = GetComponentInParent<SpriteRenderer>();
+    }
+	// Update is called once per frame
+	void OnTriggerStay2D(Collider2D c) {
+        _sr.sortingOrder = (int)c.transform.position.y * 2;
+        if (c.transform.position.y > GetComponentInParent<Transform>().position.y) {          
+            c.GetComponent<SpriteRenderer>().sortingOrder = _sr.sortingOrder - 1;
+        }
+        else {
+            c.GetComponent<SpriteRenderer>().sortingOrder = _sr.sortingOrder + 1;
+        }
+    }
+}
