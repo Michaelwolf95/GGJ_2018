64018cbd4cbaabcedc1024abf15e8a411108d8c6
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-27 01:13:10 -0800

    Playmaker (1/2)

--- GGJ_2018_Project/Assets/_Scripts/ContagionSystem/Core/IInfectable.cs
+++ GGJ_2018_Project/Assets/_Scripts/ContagionSystem/Core/IInfectable.cs
@@ -2,7 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInfectable
+namespace GGJ_2018.ContagionSystem
 {
-    
+    public struct InfectionEventArgs
+    {
+        public TransmissionMediumType TransmissionMedium;
+    }
+
+    public enum TransmissionMediumType
+    {
+        Default,
+        Contact,
+        Airborn,
+        Fluid
+    }
+
+    public delegate void InfectionEventHandler(object sender, InfectionEventArgs args);
+
+    public interface IInfectable
+    {
+        TransmissionMediumType[] MediumImmunities { get; }
+        bool TryInfect(object sender, InfectionEventArgs args);
+        event InfectionEventHandler OnInfect;
+    }
 }
---
+++ GGJ_2018_Project/Assets/_Scripts/ContagionSystem/Core/InfectableBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace GGJ_2018.ContagionSystem
+{
+    public class InfectableBase : MonoBehaviour, IInfectable
+    {
+        [SerializeField] protected TransmissionMediumType[] m_MediumImmunities;
+        public TransmissionMediumType[] MediumImmunities
+        {
+            get { return m_MediumImmunities; }
+        }
+
+        public virtual bool TryInfect(object sender, InfectionEventArgs args)
+        {
+            if (!(new List<TransmissionMediumType>(m_MediumImmunities)).Contains(args.TransmissionMedium))
+            {
+                // This should be made more complex in the future.
+                return true;
+            }
+            return false;
+        }
+
+        public event InfectionEventHandler OnInfect;
+    }
+}
