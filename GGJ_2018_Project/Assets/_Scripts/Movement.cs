4fddac38d24cd106c3f969ae1a8c7f507711c8a2
Brennan Zuber <geekssw@yahoo.com>
2018-01-27 01:28:03 -0800

    movement script
    Added character movement script

---
+++ GGJ_2018_Project/Assets/_Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Movement : MonoBehaviour {
+
+	Rigidbody2D rigidbody;
+	float x, y;
+	public float force = 10, maxVelocity = 1;
+	Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+		rigidbody.gravityScale = 0;
+		rigidbody.drag = force;
+
+		movement = new Vector2(0, 0);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		x = Input.GetAxis("Horizontal");
+		y = Input.GetAxis("Vertical");
+
+		movement.Set(x, y);
+
+		rigidbody.AddForce(movement * force);
+		rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, maxVelocity);
+	}
+}
