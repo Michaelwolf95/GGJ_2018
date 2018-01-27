0a25cdf39fdae55ffa35afecfb691faced2053b4
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-27 14:24:07 -0800

    Set up NavMesh2D AI system

---
+++ GGJ_2018_Project/Assets/_Scripts/Playmaker AI/NavMesh2D/NavMeshAgent2DStateAction.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using UnityEngine;
+
+namespace GGJ_2018.PlayMaker.NavMesh2D
+{
+
+    [ActionCategory("NavMesh2D")]
+    public abstract class NavMeshAgent2DStateAction : FsmStateAction
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(NavMeshAgent2D))]
+        [HutongGames.PlayMaker.Tooltip("The target. An Animator component is required")]
+        public FsmOwnerDefault gameObject;
+
+        protected NavMeshAgent2D navMeshAgent;
+        protected float reachedDestinationThreshold = 0.1f;
+
+        public virtual bool HasReachedDestination
+        {
+            get
+            {
+                return
+                ((Vector3.Distance(navMeshAgent.transform.position, navMeshAgent.destination) < reachedDestinationThreshold
+                  || !navMeshAgent.hasPath) && !navMeshAgent.pathPending);
+            }
+        }
+
+        public override void Reset()
+        {
+            gameObject = null;
+        }
+
+        public override void OnEnter()
+        {
+            // get the agent component
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+            if (go == null)
+            {
+                Finish();
+                return;
+            }
+
+            navMeshAgent = go.GetComponent<NavMeshAgent2D>();
+
+            if (!navMeshAgent)
+            {
+                Debug.LogWarning("NavMeshAgent2D Not Found!");
+                Finish();
+            }
+        }
+
+        protected virtual void SetDestination(Vector2 position)
+        {
+            navMeshAgent.SetDestination(position);
+        }
+        protected virtual void FollowTarget(Transform target)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
