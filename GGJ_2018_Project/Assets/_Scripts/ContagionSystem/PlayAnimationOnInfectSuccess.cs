cc461fc65a352aed967f79b18e59566f87024194
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-28 15:05:00 -0800

    Play Animation on Infect

---
+++ GGJ_2018_Project/Assets/_Scripts/ContagionSystem/PlayAnimationOnInfectSuccess.cs
@@ -0,0 +1,38 @@
+﻿using MichaelWolfGames;
+using UnityEngine;
+
+namespace GGJ_2018.ContagionSystem
+{
+    public class PlayAnimationOnInfectSuccess : SubscriberBase<InfectorBase>
+    {
+        [SerializeField] private Animator m_Animator;
+        [SerializeField] private string m_StateName = "Cough";
+        [SerializeField] private int m_Layer = 0;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            if (!m_Animator) m_Animator = GetComponent<Animator>();
+
+        }
+
+        protected override void SubscribeEvents()
+        {
+            SubscribableObject.OnInfectSuccess += PlayAnimation;
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            SubscribableObject.OnInfectSuccess -= PlayAnimation;
+        }
+
+        private void PlayAnimation()
+        {
+            if (m_Animator)
+            {
+                m_Animator.Play(m_StateName, m_Layer);
+            }
+        }
+    }
+}
