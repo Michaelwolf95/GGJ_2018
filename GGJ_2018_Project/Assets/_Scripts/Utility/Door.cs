e708e3b258d1e9dc37e824216b94b562e5eaa82a
Joel Lee <lee.joel395@gmail.com>
2018-01-28 00:09:07 -0800

    fixed plant, added door

---
+++ GGJ_2018_Project/Assets/_Scripts/Utility/Door.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+namespace GGJ_2018 {
+    public class Door : MonoBehaviour {
+        public bool player = false, patient = false, doctor = false;
+        public bool open = false;
+
+        void OnTriggerEnter(Collider c) {
+            if ((player && c.gameObject.tag == "Player")||
+                (patient && c.gameObject.tag == "Patient")||
+                (doctor && c.gameObject.tag == "Doctor")) {
+                open = true;
+                OpenDoor();
+            }
+        }
+
+        public void OpenDoor() {
+            gameObject.SetActive(false);
+        }
+    }
+}
