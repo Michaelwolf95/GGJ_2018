08360c4bed7cf4f46a30060a071dba96b8b92c00
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-27 16:31:41 -0800

    Improved NavMeshes. Placeholder characters.

---
+++ GGJ_2018_Project/Assets/_Scripts/ContagionSystem/CureOnContact.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GGJ_2018.ContagionSystem
+{
+    public class CureOnContact : MonoBehaviour
+    {
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            var go = col.collider.attachedRigidbody ? col.collider.attachedRigidbody.gameObject : col.collider.gameObject;
+            InfectableBase inf = go.GetComponent<InfectableBase>();
+            Debug.Log("Curing....");
+            if (inf != null)
+            {
+                Debug.Log("Curing!");
+                inf.CureAll();
+            }
+        }
+    }
+}
