1f28e8b06499171c81a7c64602873974037cb63b
rocketmanhs <31834217+rocketmanhs@users.noreply.github.com>
2018-01-28 14:15:08 -0800

    no message

---
+++ GGJ_2018_Project/Assets/Final_Goal_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MichaelWolfGames;
+using UnityEngine;
+namespace GGJ_2018
+{
+public class Final_Goal_Controller : MonoBehaviour
+    {
+
+        //public GameObject WinPanel;
+
+        //Game_Manager goalReached = new Game_Manager();
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!PlayerInstance.Instance)
+                return;
+            var go = col.attachedRigidbody ? col.attachedRigidbody.gameObject : col.gameObject;
+            if (go == PlayerInstance.Instance.gameObject)
+            {
+                //goalReached.YouWin(WinPanel);
+                if (Game_Manager.Instance)
+                {
+                    Game_Manager.Instance.LoadFinalLevel();
+                }
+            }
+
+
+        }
+    }
+
+}
+
+
