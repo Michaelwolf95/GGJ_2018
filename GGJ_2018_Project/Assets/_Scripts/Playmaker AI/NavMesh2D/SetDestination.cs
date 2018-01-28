90999e20b2c5244f4bcaca8b6f4142f9014a3f36
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-27 22:06:32 -0800

    SetDestination, and other edits.

--- GGJ_2018_Project/Assets/_Scripts/Playmaker AI/NavMesh2D/SetDestination.cs
+++ GGJ_2018_Project/Assets/_Scripts/Playmaker AI/NavMesh2D/SetDestination.cs
@@ -1,7 +1,32 @@
-﻿namespace GGJ_2018.PlayMaker.NavMesh2D
+﻿using HutongGames.PlayMaker;
+
+namespace GGJ_2018.PlayMaker.NavMesh2D
 {
     public class SetDestination : NavMeshAgent2DStateAction
     {
-        
+        public FsmGameObject Target;
+        public bool EveryFrame = true;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            if (Target.Value)
+            {
+                FollowTarget(Target.Value.transform);
+            }
+
+            if (!EveryFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Target.Value)
+            {
+                FollowTarget(Target.Value.transform);
+            }
+        }
     }
 }
